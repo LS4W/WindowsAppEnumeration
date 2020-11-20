d9df19e2a94040dab60c3819bddf80f66d362620
Asperand <diggly123456@gmail.com>
2020-11-19 21:14:21 -0500

    Finished clean up

---
+++ LS4W.WindowsAppEnumeration/AppEnumerator.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using LS4W.WindowsAppEnumeration.Domain;
+using Microsoft.Win32;
+using TsudaKageyu;
+
+namespace LS4W.WindowsAppEnumeration
+{
+    public class AppEnumerator
+    {
+        private readonly Configuration _config;
+
+        public AppEnumerator(Configuration config)
+        {
+            _config = config;
+        }
+
+        public IEnumerable<WindowsApp> GetWindowsApps()
+        {
+            //Get executables
+            using var key = Registry.LocalMachine.OpenSubKey(_config.AppPathRegistryKey);
+            foreach (var app in GetExecutablePaths())
+            {
+                IconExtractor ie = new IconExtractor(app.ExecutableLocation);
+                if (ie.Count == 0) //contains no icons
+                    continue;
+
+                var iconVariations = IconUtil.Split(ie.GetIcon(0));
+                app.IconPaths = CopyIcons(iconVariations, app.ExecutableLocation);
+                app.IconB64 = ConvertLargestIconToBase64(iconVariations);
+
+                yield return app;
+            }
+        }
+
+
+        private IEnumerable<WindowsApp> GetExecutablePaths()
+        {
+            using var key = Registry.LocalMachine.OpenSubKey(_config.AppPathRegistryKey);
+            foreach (var subkeyName in key.GetSubKeyNames())
+            {
+                using var subkey = key.OpenSubKey(subkeyName);
+                var rawLocation = subkey?.GetValue("");
+                var rawPath = subkey?.GetValue("Path");
+                if (!(rawLocation is string location) || !(rawPath is string path))
+                    continue;
+                if (!File.Exists(location))
+                    continue;
+                yield return new WindowsApp
+                {
+                    ExecutableLocation = location,
+                    InstallLocation = path
+                };
+            }
+        }
+
+        private List<string> CopyIcons(IEnumerable<Icon> icons, string executableLocation)
+        {
+            var paths = new List<string>();
+
+            if (!_config.CopyIcon)
+                return paths;
+
+            foreach (var (icon, index) in icons.WithIndex())
+            {
+                var iconExportName = $"{Path.GetFileName(executableLocation)}_{index}.png";
+                var iconAsBitmap = icon.ToBitmap();
+                iconAsBitmap.Save(_config.IconCopyPath + iconExportName, ImageFormat.Png);
+                paths.Add($"{_config.IconCopyPath}{iconExportName}");
+            }
+
+            return paths;
+        }
+
+        private string ConvertLargestIconToBase64(IEnumerable<Icon> icons)
+        {
+            if (!_config.ExportAsBase64)
+                return null;
+            var largestIcon = icons.OrderByDescending(icon => icon.Width).First();
+            var iconAsBitmap = largestIcon.ToBitmap();
+            using var ms = new MemoryStream();
+
+            iconAsBitmap.Save(ms, ImageFormat.Png);
+            var iconAsBytes = ms.ToArray();
+            return Convert.ToBase64String(iconAsBytes);
+        }
+    }
+}
--- LS4W.WindowsAppEnumeration/Program.cs
+++ LS4W.WindowsAppEnumeration/Program.cs
@@ -1,84 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Text.Json;
-using LS4W.WindowsAppEnumeration.Domain;
-using Microsoft.Win32;
-using TsudaKageyu;
 
 namespace LS4W.WindowsAppEnumeration
 {
     class Program
     {
-        public const string AppPathRegistryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";
+        private const string AppPathRegistryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";
+        private const bool ExportBase64 = true;
 
         static void Main(string[] args)
         {
-            bool exportBase64 = true;
-
-            List<WindowsApp> installedPrograms = new List<WindowsApp>();
-
-            //Get executables
-            using var key = Registry.LocalMachine.OpenSubKey(AppPathRegistryKey);
-            foreach (string subkeyName in key.GetSubKeyNames())
-            {
-                using (RegistryKey subkey = key.OpenSubKey(subkeyName))
-                {
-                    object rawLocation = subkey.GetValue("");
-                    object rawPath = subkey.GetValue("Path");
-                    if (rawLocation is null)
-                        continue;
-                    if (rawPath is null)
-                        continue;
-
-                    var app = new WindowsApp();
-                    app.ExecutableLocation = rawLocation.ToString();
-                    app.InstallLocation = rawPath.ToString();
-
-                    if (!File.Exists(app.ExecutableLocation))
-                        continue;
-
-                    string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                    string iconPath = assemblyPath + @"\icons\";
-                    if (!Directory.Exists(iconPath))
-                    {
-                        Directory.CreateDirectory(iconPath);
-                    }
-
-                    IconExtractor ie = new IconExtractor(app.ExecutableLocation);
-                    if (ie.Count == 0) //contains no icons
-                        continue;
-
-                    var iconVariations = IconUtil.Split(ie.GetIcon(0));
-                    foreach (var (icon, index) in iconVariations.WithIndex())
-                    {
-                        string iconExportName = $"{Path.GetFileName(app.ExecutableLocation)}_{index}.png";
-                        var iconAsBitmap = icon.ToBitmap();
-                        iconAsBitmap.Save(iconPath + iconExportName, ImageFormat.Png);
-                        app.IconPaths.Add(iconPath + iconExportName);
-                    }
-
-                    if (exportBase64)
-                    {
-                        var largestIcon = iconVariations.OrderByDescending(icon => icon.Width).First();
-                        var iconAsBitmap = largestIcon.ToBitmap();
-                        using (MemoryStream ms = new MemoryStream())
-                        {
-                            iconAsBitmap.Save(ms, ImageFormat.Png);
-                            byte[] iconAsBytes = ms.ToArray();
-                            app.IconB64 = Convert.ToBase64String(iconAsBytes);
-                        }
-                    }
-
-                    installedPrograms.Add(app);
-                }
-            }
-
+            var configuration = Configuration.GetConfiguration(args);
+            var appEnumerator = new AppEnumerator(configuration);
+            var installedPrograms = appEnumerator.GetWindowsApps();
             string jsonString = JsonSerializer.Serialize(installedPrograms);
             Console.WriteLine(jsonString);
         }
+
     }
 }
