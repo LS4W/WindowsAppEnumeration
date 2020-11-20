311e2334518840726b149096fd98358a4ad0c52e
Asperand <diggly123456@gmail.com>
2020-11-19 19:42:08 -0500

    Ported to dotnet core 3.1 also some clean up for conforming to standards

--- LS4W.WindowsAppEnumeration/ExtensionMethods.cs
+++ LS4W.WindowsAppEnumeration/ExtensionMethods.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace extensionMethods
+namespace LS4W.WindowsAppEnumeration
 {
-    public static class extensions
+    public static class Extensions
     {
         public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self)
            => self.Select((item, index) => (item, index));
